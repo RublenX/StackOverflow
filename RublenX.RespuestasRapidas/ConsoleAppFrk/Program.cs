f8c9fafec94b4263bf0b05b7389c48cf332cfa15
Rubén Pérez <ruben.perez@ciberexperis.es>
2019-06-20 10:35:03 +0200

    RublenX.RespuestasRapidas - Creación de proyectos base para contestar a las preguntas

---
+++ RublenX.RespuestasRapidas/ConsoleAppFrk/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFrk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
