51d40e3f60059a45b871dfa48f0e8ade48e53b18
Rubén Pérez <ruben.perez@ciberexperis.es>
2019-06-25 08:23:12 +0200

    RublenX.RespuestasRapidas - Respuesta a pregunta https://es.stackoverflow.com/questions/274465/c-validacion-numero-de-telefono

--- RublenX.RespuestasRapidas/ConsoleAppFrk/Program.cs
+++ RublenX.RespuestasRapidas/ConsoleAppFrk/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ConsoleAppFrk.Questions._273458;
+using ConsoleAppFrk.Questions.Q274465;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,28 @@ namespace ConsoleAppFrk
         {
             try
             {
-                LecturaXml.Lanzar();
+                //LecturaXml.Lanzar();
+
+                bool salir = false;
+
+                while (!salir)
+                {
+                    Console.WriteLine("Introduzca un número de teléfono y presione ENTER... 0 para salir de la aplicación");
+                    string telefono = Console.ReadLine();
+
+                    if (telefono == "0")
+                    {
+                        salir = true;
+                    }
+                    else if (ExpRegTelefono.Validar(telefono))
+                    {
+                        Console.WriteLine("Teléfono válido");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Teléfono inválido");
+                    }
+                }
             }
             catch (Exception ex)
             {
---
+++ RublenX.RespuestasRapidas/ConsoleAppFrk/Questions/Q274465/ExpRegTelefono.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleAppFrk.Questions.Q274465
+{
+    public class ExpRegTelefono
+    {
+        public static bool Validar(string telefono)
+        {
+            Regex exreg = new Regex("^\\d{7,10}$");
+            return exreg.IsMatch(telefono);
+        }
+    }
+}
